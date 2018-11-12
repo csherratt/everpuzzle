d898384534c3eef275f3dbc69ef873bca5315e2e
DESKTOP-PUCUJ3Q\sotde <skytrias@protonmail.com>
2018-11-12 14:36:42 +0100

    add simple block falling

--- src/systems/block_system.rs
+++ src/systems/block_system.rs
@@ -6,12 +6,18 @@ use amethyst::{
 };
 
 use basics::block::Block;
+use data::block_data::{BLOCKS, COLS};
+use std::rc::Rc;
 
-pub struct BlockSystem;
+pub struct BlockSystem {
+    assign_neighbors: bool    
+}
 
 impl BlockSystem {
     pub fn new() -> BlockSystem {
-        BlockSystem { }
+        BlockSystem { 
+            assign_neighbors: true
+        }
     }
 }
 
@@ -39,6 +45,31 @@ impl<'a> System<'a> for BlockSystem {
             }
         }
 
-        
+        let mut search_blocks = (&mut blocks).join();
+
+        // detect if any block can currently fall by looking for the
+        // bottom blocks.kind, if so set a var to true
+        for i in 0..BLOCKS - COLS {
+            let bottom_block = search_blocks.get_unchecked(i as u32).unwrap();
+            let top_block = search_blocks.get_unchecked((i + COLS) as u32).unwrap();
+
+            if bottom_block.kind == None {
+                top_block.can_fall = true;
+            } 
+        }
+
+        // if any top block can fall, switch kinds with bottom
+        // since bottom was always none - top can be none afterwards
+        for i in 0..BLOCKS - COLS {
+            let bottom_block = search_blocks.get_unchecked(i as u32).unwrap();
+            let top_block = search_blocks.get_unchecked((i + COLS) as u32).unwrap();
+
+            if top_block.can_fall {
+                bottom_block.kind = top_block.kind;
+                top_block.kind = None;
+                top_block.can_fall = false;
+            }
+        }
     }
 }
+
